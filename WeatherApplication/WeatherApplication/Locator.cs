a9dea3061f7d14fa8afc08a8cbe1192cc654ef7d
Dusan Stevic <stevicdule@gmail.com>
2019-04-09 16:17:44 +0200

    modified class locator

--- WeatherApplication/WeatherApplication/Locator.cs
+++ WeatherApplication/WeatherApplication/Locator.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Device.Location;
 
 namespace WeatherApplication
 {
     class Locator
     {
+        public double _lat { get; set; }
+        public double _lon { get; set; }
+
+        public Locator()
+        {
+            Console.WriteLine("Locating...");
+            _lat = -11111;
+            _lon = -11111;
+            locate();
+        }
+
+        public void locate()
+        {
+            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
+            GeoCoordinate coord = new GeoCoordinate();
+            long iter = 0;
+            do
+            {
+                watcher.TryStart(false, TimeSpan.FromMilliseconds(2000));
+                coord = watcher.Position.Location;
+                iter++;
+            } while (coord.IsUnknown && iter < 9999999);
+
+            if (iter < 9999999)
+            {
+                _lat = coord.Latitude;
+                _lon = coord.Longitude;
+            }
+        }
     }
 }
